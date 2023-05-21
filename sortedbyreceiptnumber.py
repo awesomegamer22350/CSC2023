5b1e2b74f6756b33c72e0722070c38076b20bed2
Shun <awesomegamer22350@gmail.com>
2023-05-22 11:05:25 +1200

    Edit Mode

--- sortedbyreceiptnumber.py
+++ sortedbyreceiptnumber.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import ttk
+from datetime import datetime
 
 root = Tk()
 root.title("Julie's Party")
@@ -8,18 +10,44 @@ def removefromgrid(removethis):
 	removethis.grid_remove()
 
 def update(rownum, btnid):
-	if len(editEntrybox.get()) >= 23:
-		toolonglbl.grid()
-		root.after(3000, lambda: removefromgrid(toolonglbl))
-		return
-	#if btnid==3:
-		if Entrybox.get().strip().isdigit() == FALSE:
-			notanintlbl.grid()
-			root.after(3000, lambda: removefromgrid(notanintlbl))
-			return
 	for x in range(len(biglist)):
 		if biglist[x][0]==rownum:
 			biglist[x][1][btnid-1][0].configure(text=editEntrybox.get())
+			biglist[x][1][4][0].configure(text=datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+
+			if btnid==1:
+				biglist[x][1][0][1].configure(text=f'Update Receipt {editEntrybox.get()}')
+				del sortlist[x]
+				pp=biglist.pop(x)
+
+				receiptnum=int(editEntrybox.get())
+				print(receiptnum)
+				receiptposition=-1; a=0
+				for receiptposition, y in enumerate(sortlist):
+					if receiptnum<y:
+						a=1
+						break
+				if a==0:
+					receiptposition+=1
+				sortlist.insert(receiptposition, receiptnum)
+				biglist.insert(receiptposition, pp)
+				for x, y in enumerate(biglist):
+					for p, q in enumerate(y[1]):
+						q[0].grid(row=3*x+1, column=p)
+						q[1].grid(row=3*x+2, column=p)
+					y[2].grid(row=3*x+2)
+					y[3].grid(row=3*x+3)
+	#if len(editEntrybox.get()) >= 23:
+	#	toolonglbl.grid()
+	#	root.after(3000, lambda: removefromgrid(toolonglbl))
+	#	return
+	#if btnid==3:
+	#	if Entrybox.get().strip().isdigit() == FALSE:
+	#		notanintlbl.grid()
+	#		root.after(3000, lambda: removefromgrid(notanintlbl))
+	#		return
+	updateScrollRegion()
+
 
 def log(*args):
 	global identification
@@ -34,10 +62,7 @@ def log(*args):
 			break
 	if a==0:
 		receiptposition+=1
-
-	print(receiptposition)
 	sortlist.insert(receiptposition, receiptnum)
-	print(sortlist)
 	biglist.insert(receiptposition, [rownum, [
 	[Label(Frame3, text=receiptnum, font=("Arial", 13, "bold"), height=1),
 	Button(Frame3, text=f"Update Receipt {receiptnum}", command=lambda btnid=1: update(rownum, btnid))],
@@ -47,38 +72,106 @@ def log(*args):
 	Button(Frame3, text=f"Update Item", command=lambda btnid=3: update(rownum, btnid))],
 	[Label(Frame3, text=NumHiredEntrybox.get()),
 	Button(Frame3, text=f"Update hired no.", command=lambda btnid=4: update(rownum, btnid))],
-	[Label(Frame3, text=""),
+	[Label(Frame3, text=datetime.now().strftime("%d/%m/%Y %H:%M:%S")),
   	Button(Frame3, text="Delete Receipt", command=lambda: deletegroup(rownum))]
 	], 
-	Label(Frame3, text='', height=2)])
-
+	Label(Frame3, text='', height=2), ttk.Separator(Frame3)])
 	for x, y in enumerate(biglist):
 		for p, q in enumerate(y[1]):
-			q[0].grid(row=2*x+1, column=p)
-			q[1].grid(row=2*x+2, column=p)
-		y[2].grid(row=2*x+2, column=5)
+			q[0].grid(row=3*x+1, column=p)
+			q[1].grid(row=3*x+2, column=p)
+			if isedit==0 and p!=4:
+				q[1].grid_remove()
+		y[2].grid(row=3*x+2, column=5)
+		y[3].grid(row=3*x+3, column=0, columnspan=999, sticky="ew")
 
 	updateScrollRegion()
+
 
 # delete a group
 def deletegroup(row):
 	# tracks that this group has been deleted
 	for x in range(len(biglist)):
 		if biglist[x][0] == row:
-			saved = biglist[x][1]
+			saved = biglist[x]
 			del sortlist[x]
 			del biglist[x]
 			break
 	# update text
 	for x, y in enumerate(biglist):
-		for q in y[1]:
-			q[0].grid(row=2*x+1)
-			q[1].grid(row=2*x+2)
-		y[2].grid(row=2*x+2)
-	for line in saved:
-		line[0].grid_remove()
-		line[1].grid_remove()
+		for p, q in enumerate(y[1]):
+			q[0].grid(row=3*x+1)
+			q[1].grid(row=3*x+2)
+			if isedit==0 and p!=4:
+				q[1].grid_remove()
+		y[2].grid(row=3*x+2)
+		y[3].grid(row=3*x+3)
+	for line in saved[1]:
+		line[0].destroy()
+		line[1].destroy()
+	saved[2].destroy()
+	saved[3].destroy()
+
 	updateScrollRegion()
+
+
+
+def editmode():
+	global isedit
+	isedit=1
+	Receiptlbl.grid_remove()
+	ReceiptEntrybox.grid_remove()
+	Customernamelbl.grid_remove()
+	CustomernameEntrybox.grid_remove()
+	ItemHiredlbl.grid_remove()
+	ItemHiredEntrybox.grid_remove()
+	NumHiredlbl.grid_remove()
+	NumHiredEntrybox.grid_remove()
+	Logbtn.grid_remove()
+	editTextbox.grid()
+	editEntrybox.grid()
+	editbtn.place_forget()
+	exitEdit.place(x=0, y=750)
+	for x in biglist:
+		for y in x[1]:
+			y[1].grid()
+
+def exitedit():
+	global isedit
+	isedit=0
+	editTextbox.grid_remove()
+	editEntrybox.grid_remove()
+	Receiptlbl.grid()
+	ReceiptEntrybox.grid()
+	Customernamelbl.grid()
+	CustomernameEntrybox.grid()
+	ItemHiredlbl.grid()
+	ItemHiredEntrybox.grid()
+	NumHiredlbl.grid()
+	NumHiredEntrybox.grid()
+	Logbtn.grid()
+	editbtn.place(x=0, y=750)
+	exitEdit.place_forget()
+	for x in biglist:
+		for y, z in enumerate(x[1]):
+			if y!=4:
+				z[1].grid_remove()
+
+def validity():
+	# Label for invalid input for integers
+	notanintlbl = Label(Frame1, text="Not an int or is negative", fg='red')
+	notanintlbl.grid(row=0, column=2)
+	notanintlbl.grid_remove()
+
+	# Label for inputs that are too long
+	toolonglbl = Label(Frame1, text="your input is too long, it must be under 23 characters", fg='red')
+	toolonglbl.grid(row=0, column=2)
+	toolonglbl.grid_remove()
+
+	# Label for input if not between 5-10 campers
+	outsiderangelbl = Label(Frame1, text="Warning: there should be between 5-10 campers", fg='red')
+	outsiderangelbl.grid(row=0, column=3)
+	outsiderangelbl.grid_remove()
 
 def updateScrollRegion():
 	canvas.update_idletasks()
@@ -97,11 +190,11 @@ def unbindToCanvas(*args):
 
 
 
-categorylist = ["Receipt no.", "Customer Name: ", "Item Hired: ", "Number of Items Hired: "]
+categorylist = ["Receipt no.", "Customer Name: ", "Item Hired: ", "Number of Items Hired: ", "Last Updated: "]
 biglist = []
 sortlist = []
 identification=-1
-
+isedit=0
 
 Frame1 = Frame(root)
 Frame1.place(x=0, y=5)
@@ -130,36 +223,19 @@ NumHiredlbl.grid(row=0, column=6)
 NumHiredEntrybox = Entry(Frame1)
 NumHiredEntrybox.grid(row=0, column=7)
 
-Logbtn = Button(Frame1, text= 'Log Receipt', command= log)
+Logbtn = Button(Frame1, text= 'Log Receipt', command=log)
 Logbtn.grid(row=0, column=8)
 
 
 
 editTextbox = Label(Frame1, text='Input below and then press an "update" button')
 editTextbox.grid(row=0, column=0)
-editTextbox.grid_remove()
 
 
 editEntrybox = Entry(Frame1)
 editEntrybox.grid(row=1, column=0)
-editEntrybox.grid_remove()
 
-
-
-# Label for invalid input for integers
-notanintlbl = Label(Frame1, text="Not an int or is negative", fg='red')
-notanintlbl.grid(row=0, column=2)
-notanintlbl.grid_remove()
-
-# Label for inputs that are too long
-toolonglbl = Label(Frame1, text="your input is too long, it must be under 23 characters", fg='red')
-toolonglbl.grid(row=0, column=2)
-toolonglbl.grid_remove()
-
-# Label for input if not between 5-10 campers
-outsiderangelbl = Label(Frame1, text="Warning: there should be between 5-10 campers", fg='red')
-outsiderangelbl.grid(row=0, column=3)
-outsiderangelbl.grid_remove()
+#validity()
 
 # Frame 2 keeps the grid of the below stuff in different columns than the stuff in Frame 1
 Frame2 = Frame(root)
@@ -182,7 +258,11 @@ Frame3.bind("<Leave>", lambda event: unbindToCanvas())
 
 # Text for Catagories
 for i in range(len(categorylist)):
-	Label(Frame3, text=categorylist[i], width=20, height=2, font=("Arial", 14, "bold"),anchor=W).grid(row=0, column=i)
+	Label(Frame3, text=categorylist[i], width=20, height=2, font=("Arial", 14, "bold")).grid(row=0, column=i)
 
+
+editbtn = Button(root, text="Edit", command=editmode)
+exitEdit = Button(root, text="Exit Edit", command=exitedit)
+exitedit()
 
 root.mainloop()
